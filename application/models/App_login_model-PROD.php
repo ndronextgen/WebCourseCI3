fa8a66876696eb751cc9d2f4ee9180421c96da00
Joe Kusuma <hendranextge@yahoo.com>
2023-01-10 14:31:46 +0700

    backup file.

--- application/models/App_login_model-PROD.php
+++ application/models/App_login_model-PROD.php
@@ -1,7 +1,6 @@
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class App_Login_Model extends CI_Model
-{
+class App_Login_Model extends CI_Model {
 
 	/*
 		***	Model : app_login_model.php
@@ -10,10 +9,12 @@ class App_Login_Model extends CI_Model
 	public function getLoginData($data)
 	{
 		$login['username'] = $data['username'];
-		$login['password'] = md5($data['password'] . 'AppSimpeg32');
+		$login['password'] = md5($data['password'].'AppSimpeg32');
 		$cek = $this->db->get_where('tbl_user_login', $login);
-		if ($cek->num_rows() > 0) {
-			foreach ($cek->result() as $qad) {
+		if($cek->num_rows()>0)
+		{
+			foreach($cek->result() as $qad)
+			{
 				$sess_data['logged_in'] = 'yesGetMeLoginBaby';
 				$sess_data['id_user'] = $qad->id_user_login;
 				$sess_data['id_pegawai'] = $qad->id_pegawai;
@@ -26,42 +27,61 @@ class App_Login_Model extends CI_Model
 				$sess_data['lokasi_kerja'] = $qad->id_lokasi_kerja;
 
 				$foto = base_url() . 'asset/foto_pegawai/no-image/nofoto.png';
+				$sess_data['isUserShowPopup'] = [];
 				if ($qad->id_pegawai != 0) {
-					$q = $this->db->get_where("tbl_data_pegawai", $qad->id_pegawai);
+					$q = $this->db->get_where("tbl_data_pegawai", ["id_pegawai" => $qad->id_pegawai]);
 					if ($q->num_rows() > 0) {
-						foreach ($q->result() as $row) {
+						$row = $q->row();
+						if (!empty($row->foto)) {
 							$foto = base_url() . 'asset/foto_pegawai/thumb/' . $row->foto;
+						}
+
+						$sess_data['alreadyOpenPopup'] = $qad->is_show_popup;
+						if (date("d") >= "01" && date("Y-m-d", strtotime($row->tgl_show_popup)) > date("Y-m-d")) {
+							$this->reupdate_is_show_popup($qad->id_pegawai);
+							$sess_data['alreadyOpenPopup'] = 0;
+						}
+
+						$config_popup = $this->db->get_where('config_popup');
+						if ($config_popup->num_rows() > 0) {
+							foreach ($config_popup->result() as $pop) :
+								$type = $pop->type == "pegawai" ? "id_" . $pop->type : $pop->type;
+								if (in_array($row->{$type}, explode(",", $pop->value))) :
+									$sess_data['isUserShowPopup'][] = 1;
+								endif;
+							endforeach;
 						}
 					}
 				}
 				$sess_data['foto'] = $foto;
 
-				$this->session->set_userdata($sess_data);
+				$this->session->set_userdata($sess_data); 
 				setcookie('lokasi_kerja', $qad->id_lokasi_kerja);
 			}
 			$this->session->set_flashdata('welcome', 'Silahkan Lakukan Update Data Anda Secara Lengkap dan Sesuai');
-			header('location:' . base_url() . '');
-		} else {
+			header('location:'.base_url().'');
+		}
+		else
+		{
 			$this->session->set_flashdata('result_login', "Maaf, kombinasi username dan password yang anda masukkan tidak valid dengan database kami.");
-			header('location:' . base_url() . '');
+			header('location:'.base_url().'');
 		}
 	}
 
-	public function GetLoginDataSSO($data)
-	{
+	public function GetLoginDataSSO($data) {
 		$this->load->helper('sso');
-		log_message('debug', 'GetLoginData');
+		log_message('debug','GetLoginData');
 
 		//get token from sso
 		$ssoToken = SSOGetToken($data['username'], $data['password']);
-		log_message('debug', 'ssoToken : ' . json_encode($ssoToken));
+		log_message('debug','ssoToken : '.json_encode($ssoToken));
 		if ($ssoToken['status']) {
 			$ssoUser = SSOLogin($ssoToken['token']);
-			log_message('debug', 'ssoUser : ' . json_encode($ssoUser));
+			log_message('debug','ssoUser : '.json_encode($ssoUser));
 			if ($ssoUser['status']) {
 				//get detail user
 				$detailUser = $this->GetDetailUser($data['username']);
-				log_message('debug', 'detailUser : ' . json_encode($detailUser));
+				log_message('debug','detailUser : '.json_encode($detailUser));
 				if ($detailUser['status']) {
 					$sess_data['logged_in'] = 'yesGetMeLoginBaby';
 					$sess_data['id_user'] = $detailUser['data']['id_user'];
@@ -76,49 +96,63 @@ class App_Login_Model extends CI_Model
 					$sess_data['foto'] = $detailUser['data']['foto'];
 
 					//set si-adik cookie
-					$this->session->set_userdata($sess_data);
+					$this->session->set_userdata($sess_data); 
 
 					//set cookie sso
 					setcookie('sso_dcktrp', $ssoToken['token'], null, '/', 'jakarta.go.id');
 
-					if ($sess_data['stts'] == "administrator") {
-						log_message('debug', 'administrator');
-						header('location:' . base_url() . 'admin/dashboard_admin');
-					} else if ($sess_data['stts'] == "publik" && $sess_data['password'] == md5('123456AppSimpeg32')) {
-						//$this->session->set_flashdata('welcome', 'Silahkan Lakukan Update Data Anda Secara Lengkap dan Sesuai');
-						header('location:' . base_url() . 'app/change_password_publik');
-					} else if ($sess_data['stts'] == "publik") {
-						//$this->session->set_flashdata('welcome', 'Silahkan Lakukan Update Data Anda Secara Lengkap dan Sesuai');
-						header('location:' . base_url() . 'dashboard_publik');
-					} else if ($sess_data['stts'] == "executive") {
-						header('location:' . base_url() . 'dashboard_publik');
+					if ($sess_data['stts']=="administrator") {
+						log_message('debug','administrator');
+						header('location:'.base_url().'admin/dashboard_admin');
 					}
-				} else {
-					$this->session->set_flashdata('result_login', "Maaf, kombinasi username dan password yang anda masukkan tidak valid dengan database kami.");
-					header('location:' . base_url() . '');
+					else if ($sess_data['stts']=="publik" && $sess_data['password']==md5('123456AppSimpeg32')) {
+						//$this->session->set_flashdata('welcome', 'Silahkan Lakukan Update Data Anda Secara Lengkap dan Sesuai');
+						header('location:'.base_url().'app/change_password_publik');
+					}
+					else if ($sess_data['stts']=="publik") {
+						//$this->session->set_flashdata('welcome', 'Silahkan Lakukan Update Data Anda Secara Lengkap dan Sesuai');
+						header('location:'.base_url().'dashboard_publik');
+					}
+					else if ($sess_data['stts']=="executive") {
+						header('location:'.base_url().'dashboard_publik');
+					}
 				}
-			} else {
-				$this->session->set_flashdata('result_login', "Maaf, kombinasi username dan password yang anda masukkan tidak valid dengan database kami.");
-				header('location:' . base_url() . '');
+				else {
+					$this->session->set_flashdata('result_login', "Maaf, kombinasi username dan password yang anda masukkan tidak valid dengan database kami.");
+					header('location:'.base_url().'');
+				}
 			}
-		} else {
+			else {
+				$this->session->set_flashdata('result_login', "Maaf, kombinasi username dan password yang anda masukkan tidak valid dengan database kami.");
+				header('location:'.base_url().'');
+			}
+		}
+		else {
 			$this->session->set_flashdata('result_login', "Maaf, kombinasi username dan password yang anda masukkan tidak valid dengan database kami.");
-			header('location:' . base_url() . '');
+			header('location:'.base_url().'');
 		}
 	}
 
-	public function GetDetailUser($username)
+	function reupdate_is_show_popup($id, $value = 0)
 	{
+		$this->db->update("tbl_data_pegawai", [
+			"is_show_popup"  => $value,
+			"tgl_show_popup" => date("Y-m-d"),
+		], ["id_pegawai" => $id]);
+		return $this->db->affected_rows();
+	}
+
+	public function GetDetailUser($username) {
 		$status = false;
 		$data = null;
 		$message = '';
 
 		//get detail user
 		$objUser = $this->db->get_where('tbl_user_login', ['username' => $username]);
-		if ($objUser->num_rows() > 0) {
+		if ($objUser->num_rows()>0) {
 			$status = true;
 
-			foreach ($objUser->result() as $qad) {
+			foreach($objUser->result() as $qad) {
 				$sess_data['logged_in'] = 'yesGetMeLoginBaby';
 				$data['id_user'] = $qad->id_user_login;
 				$data['id_pegawai'] = $qad->id_pegawai;
@@ -130,18 +164,19 @@ class App_Login_Model extends CI_Model
 				$data['telepon'] = $qad->telepon;
 				$data['lokasi_kerja'] = $qad->id_lokasi_kerja;
 
-				$foto = base_url() . 'asset/foto_pegawai/no-image/nofoto.png';
+				$foto = base_url().'asset/foto_pegawai/no-image/nofoto.png';
 				if ($qad->id_pegawai != 0) {
-					$q = $this->db->get_where("tbl_data_pegawai", $qad->id_pegawai);
+					$q = $this->db->get_where("tbl_data_pegawai",$qad->id_pegawai);
 					if ($q->num_rows() > 0) {
 						foreach ($q->result() as $row) {
-							$foto = base_url() . 'asset/foto_pegawai/thumb/' . $row->foto;
+							$foto = base_url().'asset/foto_pegawai/thumb/'.$row->foto;
 						}
 					}
 				}
 				$data['foto'] = $foto;
 			}
-		} else {
+		}
+		else {
 			$message = 'Failed get user detail.';
 		}
 
