53189b25e2ed7eb7a9a1b66c08a8c529648f3bb4
Joe Kusuma <hendranextge@yahoo.com>
2023-01-18 15:06:01 +0700

    label new manual.

--- application/views/dashboard_publik/template/navbar.php
+++ application/views/dashboard_publik/template/navbar.php
@@ -1,3 +1,18 @@
+<!-- LABEL NEW -->
+<style type="text/css">
+    .label-new {
+        background-color: yellow;
+        color: red;
+    }
+
+    .label-new-child {
+        background-color: yellow;
+        color: red;
+        font-size: xx-small;
+    }
+</style>
+
+<!-- BEGIN: ACTIVE MENU -->
 <?php
 $aktif_beranda = '';
 $aktif_arsip_digital = '';
@@ -53,6 +68,7 @@ if ($menu == 'beranda') {
     $aktif_verifikasi_7 = 'active';
 }
 ?>
+<!-- END: ACTIVE MENU -->
 
 <header class="main-header">
     <nav class="navbar navbar-fixed-top">
@@ -71,6 +87,7 @@ if ($menu == 'beranda') {
 
                     <li class="dropdown user user-menu <?= $aktif_kertas_kerja ?>">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                            <span class="label label-new">&nbsp; Baru! &nbsp;</span>&nbsp;
                             <span class="hidden-xs">Kertas Kerja &nbsp;
                                 <?php if ($count_see > 0 or $count_see_tj > 0 or $count_see_kaku > 0) { ?>
                                     <span class="badge btn-warning btn-flat" id="ttl_kertas_kerja"><?php echo $count_see + $count_see_tj + $count_see_kaku; ?></span>
@@ -88,6 +105,8 @@ if ($menu == 'beranda') {
                             </li>
                             <li class="<?= $aktif_kertas_kerja_2 ?>">
                                 <a href="<?php echo base_url(); ?>tunjangan"><i class="icon-leaf icon-white"></i>Surat Permohonan Tunjangan Keluarga &nbsp;
+                                    <!-- <small class="badge" style="font-size: 9px;padding: 5px;background: #f7e928;color:#d9351c;">Baru!</small>&nbsp; -->
+                                    <span class="badge label-new-child">&nbsp; Baru! &nbsp;</span>&nbsp;
                                     <?php if ($count_see_tj > 0) { ?>
                                         <span class="badge btn-warning btn-flat" id="notif_count_tunjangan"><?php echo '' . $count_see_tj; ?></span>
                                     <?php } ?>
@@ -96,6 +115,7 @@ if ($menu == 'beranda') {
 
                             <li class="<?= $aktif_kertas_kerja_3 ?>">
                                 <a href="<?php echo base_url(); ?>kariskarsu"><i class="icon-leaf icon-white"></i>Surat Permohonan KARIS/KARSU &nbsp;
+                                    <span class="badge label-new-child">&nbsp; Baru! &nbsp;</span>&nbsp;
                                     <?php if ($count_see_kaku > 0) { ?>
                                         <span class="badge btn-warning btn-flat" id="notif_count_kariskarsu"><?php echo '' . $count_see_kaku; ?></span>
                                     <?php } ?>
@@ -105,7 +125,8 @@ if ($menu == 'beranda') {
                     </li>
 
                     <li class="<?= $aktif_lapor ?>">
-                        <a href="<?php echo base_url(); ?>Lapor"><i class="icon-home icon-white"></i> Lapor
+                        <a href="<?php echo base_url(); ?>Lapor"><i class="icon-home icon-white"></i> Lapor &nbsp;
+                            <span class="label label-new">&nbsp; Baru! &nbsp;</span>&nbsp;
                             <?php if ($count_see_lapor > 0) { ?>
                                 <span class="badge btn-warning btn-flat" id="notify_lapor"><?php echo '' . $count_see_lapor; ?></span>
                             <?php } ?>
@@ -115,6 +136,7 @@ if ($menu == 'beranda') {
                     <?php if ($status_user == 'true') { ?>
                         <li class="dropdown user user-menu <?= $aktif_verifikasi ?>">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                <span class="label label-new">&nbsp; Baru! &nbsp;</span>&nbsp;
                                 <span class="hidden-xs">Verifikasi &nbsp;
                                     <?php if (
                                             $count_see_verifikasi > 0
@@ -126,18 +148,19 @@ if ($menu == 'beranda') {
                                             or $count_see_verifikasi_pindah_tugas > 0
                                         ) { ?>
                                         <span class="badge btn-warning btn-flat" id="ttl_verifikasi"><?php echo $count_see_verifikasi
-                                                                                                + $count_see_verifikasi_tj
-                                                                                                + $count_see_verifikasi_kaku
-                                                                                                + $count_see_verifikasi_hukdis
-                                                                                                + $count_see_verifikasi_tp
-                                                                                                + $count_see_verifikasi_karir
-                                                                                                + $count_see_verifikasi_pindah_tugas; ?></span>
+                                                                                                                    + $count_see_verifikasi_tj
+                                                                                                                    + $count_see_verifikasi_kaku
+                                                                                                                    + $count_see_verifikasi_hukdis
+                                                                                                                    + $count_see_verifikasi_tp
+                                                                                                                    + $count_see_verifikasi_karir
+                                                                                                                    + $count_see_verifikasi_pindah_tugas; ?></span>
                                     <?php } ?>
                                     <i class="caret"></i></span>
                             </a>
                             <ul class="dropdown-menu">
                                 <li class="<?= $aktif_verifikasi_1 ?>">
                                     <a href="<?php echo base_url(); ?>verifikasi"><i class="icon-off"></i> Verifikasi Surat Keterangan Pegawai &nbsp;
+                                        <span class="badge label-new-child">&nbsp; Baru! &nbsp;</span>&nbsp;
                                         <?php if ($count_see_verifikasi > 0) { ?>
                                             <span class="badge btn-warning btn-flat" id="notif_count_verifikasi"><?php echo '' . $count_see_verifikasi; ?></span>
                                         <?php } ?>
@@ -145,6 +168,7 @@ if ($menu == 'beranda') {
                                 </li>
                                 <li class="<?= $aktif_verifikasi_2 ?>">
                                     <a href="<?php echo base_url(); ?>verifikasi_tunjangan"><i class="icon-off"></i> Verifikasi Surat Permohonan Tunjangan Keluarga &nbsp;
+                                        <span class="badge label-new-child">&nbsp; Baru! &nbsp;</span>&nbsp;
                                         <?php if ($count_see_verifikasi_tj > 0) { ?>
                                             <span class="badge btn-warning btn-flat" id="notif_count_verifikasi_tj"><?php echo '' . $count_see_verifikasi_tj; ?></span>
                                         <?php } ?>
@@ -152,6 +176,7 @@ if ($menu == 'beranda') {
                                 </li>
                                 <li class="<?= $aktif_verifikasi_3 ?>">
                                     <a href="<?php echo base_url(); ?>verifikasi_kariskarsu"><i class="icon-off"></i> Verifikasi Surat Permohonan KARIS/KARSU &nbsp;
+                                        <span class="badge label-new-child">&nbsp; Baru! &nbsp;</span>&nbsp;
                                         <?php if ($count_see_verifikasi_kaku > 0) { ?>
                                             <span class="badge btn-warning btn-flat" id="notif_count_verifikasi_kaku"><?php echo '' . $count_see_verifikasi_kaku; ?></span>
                                         <?php } ?>
@@ -166,6 +191,7 @@ if ($menu == 'beranda') {
                                         ?>
                                     <li class="<?= $aktif_verifikasi_4 ?>">
                                         <a href="<?php echo base_url(); ?>verifikasi_hukdis"><i class="icon-off"></i> Verifikasi Surat Keterangan Hukuman Disiplin &nbsp;
+                                            <span class="badge label-new-child">&nbsp; Baru! &nbsp;</span>&nbsp;
                                             <?php if ($count_see_verifikasi_hukdis > 0) { ?>
                                                 <span class="badge btn-warning btn-flat" id="notif_count_verifikasi_hukdis"><?php echo '' . $count_see_verifikasi_hukdis; ?></span>
                                             <?php } ?>
@@ -173,6 +199,7 @@ if ($menu == 'beranda') {
                                     </li>
                                     <li class="<?= $aktif_verifikasi_5 ?>">
                                         <a href="<?php echo base_url(); ?>verifikasi_tindak_pidana"><i class="icon-off"></i> Verifikasi Surat Keterangan Bebas Tindak Pidana &nbsp;
+                                            <span class="badge label-new-child">&nbsp; Baru! &nbsp;</span>&nbsp;
                                             <?php if ($count_see_verifikasi_tp > 0) { ?>
                                                 <span class="badge btn-warning btn-flat" id="notif_count_verifikasi_tp"><?php echo '' . $count_see_verifikasi_tp; ?></span>
                                             <?php } ?>
@@ -180,6 +207,7 @@ if ($menu == 'beranda') {
                                     </li>
                                     <li class="<?= $aktif_verifikasi_6 ?>">
                                         <a href="<?php echo base_url(); ?>verifikasi_pengembangan_karir"><i class="icon-off"></i> Verifikasi Surat Kebutuhan Pengembangan Karir &nbsp;
+                                            <span class="badge label-new-child">&nbsp; Baru! &nbsp;</span>&nbsp;
                                             <?php if ($count_see_verifikasi_karir > 0) { ?>
                                                 <span class="badge btn-warning btn-flat" id="notif_count_verifikasi_karir"><?php echo '' . $count_see_verifikasi_karir; ?></span>
                                             <?php } ?>
@@ -187,6 +215,7 @@ if ($menu == 'beranda') {
                                     </li>
                                     <li class="<?= $aktif_verifikasi_7 ?>">
                                         <a href="<?php echo base_url(); ?>verifikasi_pindah_tugas"><i class="icon-off"></i> Verifikasi Surat Kebutuhan Pindah Tugas &nbsp;
+                                            <span class="badge label-new-child">&nbsp; Baru! &nbsp;</span>&nbsp;
                                             <?php if ($count_see_verifikasi_pindah_tugas > 0) { ?>
                                                 <span class="badge btn-warning btn-flat" id="notif_count_verifikasi_pindah_tugas"><?php echo '' . $count_see_verifikasi_pindah_tugas; ?></span>
                                             <?php } ?>
@@ -197,14 +226,33 @@ if ($menu == 'beranda') {
                         </li>
                     <?php } ?>
 
-                    <li class=""><a href="<?php echo base_url(); ?>dashboard_publik/download_manualbook" title="Download Panduan Penggunaan"><i class="icon-home icon-white"></i> Panduan</a></li>
+                    <li class="">
+                        <a href="<?php echo base_url(); ?>dashboard_publik/download_manualbook" title="Download Panduan Penggunaan"><i class="icon-home icon-white"></i> Panduan &nbsp;
+                            <span class="label label-new">&nbsp; Baru! &nbsp;</span>&nbsp;
+                        </a>
+                    </li>
 
                     <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-comment icon-white"></i> Pedoman<b class="caret"></b></a>
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-comment icon-white"></i> Pedoman&nbsp;
+                            <span class="label label-new">&nbsp; Baru! &nbsp;</span>&nbsp;
+                            <b class="caret"></b>
+                        </a>
                         <ul class="dropdown-menu">
-                            <li><a href="<?php echo base_url(); ?>dashboard_publik/download_pedoman_3" target=""><i class="icon-fire"></i> SK Kepala Dinas Update Data SI-ADiK</a></li>
-                            <li><a href="<?php echo base_url(); ?>dashboard_publik/download_pedoman_1" target=""><i class="icon-fire"></i> Permendikbud RI No. 50 Tahun 2015</a></li>
-                            <li><a href="<?php echo base_url(); ?>dashboard_publik/download_pedoman_2" target=""><i class="icon-fire"></i> Pergub DKI Jakarta No. 99 Tahun 2021</a></li>
+                            <li>
+                                <a href="<?php echo base_url(); ?>dashboard_publik/download_pedoman_3" target=""><i class="icon-fire"></i> SK Kepala Dinas Update Data SI-ADiK&nbsp;
+                                    <span class="badge label-new-child">&nbsp; Baru! &nbsp;</span>&nbsp;
+                                </a>
+                            </li>
+                            <li>
+                                <a href="<?php echo base_url(); ?>dashboard_publik/download_pedoman_1" target=""><i class="icon-fire"></i> Permendikbud RI No. 50 Tahun 2015&nbsp;
+                                    <span class="badge label-new-child">&nbsp; Baru! &nbsp;</span>&nbsp;
+                                </a>
+                            </li>
+                            <li>
+                                <a href="<?php echo base_url(); ?>dashboard_publik/download_pedoman_2" target=""><i class="icon-fire"></i> Pergub DKI Jakarta No. 99 Tahun 2021&nbsp;
+                                    <span class="badge label-new-child">&nbsp; Baru! &nbsp;</span>&nbsp;
+                                </a>
+                            </li>
                         </ul>
                     </li>
 
@@ -236,7 +284,8 @@ if ($menu == 'beranda') {
                 </ul>
             </div>
 
-            <?php // echo menuHtml(); ?>
+            <?php // echo menuHtml(); 
+            ?>
 
         </div>
     </nav>
