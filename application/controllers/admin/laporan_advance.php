bccf214e3a1ff018b780e0001b978e49eea561e9
wongndro <ndra.yoga@gmail.com>
2023-01-29 16:54:13 +0700

    laporan advance cetak

--- application/controllers/admin/laporan_advance.php
+++ application/controllers/admin/laporan_advance.php
@@ -42,6 +42,11 @@ class Laporan_advance extends CI_Controller
 					}
 				}
 			}
+			// -----
+			$d['golongan'] 			= $this->db->query("SELECT * FROM tbl_master_golongan ORDER BY id_golongan ASC")->result();
+			$d['jenis_kelamin']	 	= $this->db->query("SELECT * FROM tbl_master_jenis_kelamin ORDER BY Id ASC")->result();
+			$d['status_pegawai'] 	= $this->db->query("SELECT * FROM tbl_master_status_pegawai ORDER BY no_urut ASC")->result();
+			// -----
 
 			$d['arrLokasi'] = $arrLokasi;
 			$d['arrLokasiSelected'] = $arrLokasiSelected;
@@ -58,7 +63,10 @@ class Laporan_advance extends CI_Controller
 	{
 		$data = [
 			'lokasi' => $this->input->post('lokasi'),
-			'sublokasi' => $this->input->post('sublokasi')
+			'sublokasi' => $this->input->post('sublokasi'),
+			'id_golongan' => $this->input->post('id_golongan'),
+			'status_pegawai' => $this->input->post('status_pegawai'),
+			'jenis_kelamin' => $this->input->post('jenis_kelamin')
 		];
 
 		$this->load->view('dashboard_admin/laporan/advance/ajax_table', $data);
@@ -68,9 +76,12 @@ class Laporan_advance extends CI_Controller
 	{
 		$lokasi = $this->input->post('lokasi');
 		$sublokasi = $this->input->post('sublokasi');
+		$id_golongan = $this->input->post('id_golongan');
+		$status_pegawai = $this->input->post('status_pegawai');
+		$jenis_kelamin = $this->input->post('jenis_kelamin');
 
-		$listing 		= $this->laporan_advance->listing($lokasi, $sublokasi);
-		$jumlah_filter 	= $this->laporan_advance->jumlah_filter($lokasi, $sublokasi);
+		$listing 		= $this->laporan_advance->listing($lokasi, $sublokasi, $id_golongan, $status_pegawai, $jenis_kelamin);
+		$jumlah_filter 	= $this->laporan_advance->jumlah_filter($lokasi, $sublokasi, $id_golongan, $status_pegawai, $jenis_kelamin);
 		$jumlah_semua 	= $this->laporan_advance->jumlah_semua();
 
 		$data = array();
@@ -85,6 +96,9 @@ class Laporan_advance extends CI_Controller
 			$row[] = $key->nip;
 			$row[] = $key->nrk;
 			$row[] = $key->nama_pegawai;
+			$row[] = $key->golongan;
+			$row[] = $key->nama_status;
+			$row[] = $key->jenis_kelamin;
 			$row[] = $key->lokasi_kerja;
 			$row[] = $key->sublokasi_kerja;
 
@@ -114,6 +128,51 @@ class Laporan_advance extends CI_Controller
 			echo json_encode($result);
 		}
 	}
+
+	public function cetak() {
+		//	sama dengan yg di fungsi filter
+		$lokasi = $this->input->get('lokasi');
+		$sublokasi = $this->input->get('sublokasi');
+		$id_golongan = $this->input->get('id_golongan');
+		$status_pegawai = $this->input->get('status_pegawai');
+		$jenis_kelamin = $this->input->get('jenis_kelamin');
+
+		$ctype		= $this->input->get('ctype');
+		$Tahun_Now  = date('Y');
+		$Bulan_Now  = date('m');
+		//
+		$a['lokasi'] 		= $this->input->get('lokasi');
+		$a['sublokasi'] 	= $this->input->get('sublokasi');
+		$a['id_golongan'] 	= $this->input->get('id_golongan');
+		$a['status_pegawai'] = $this->input->get('status_pegawai');
+		$a['jenis_kelamin'] = $this->input->get('jenis_kelamin');
+		$a['ctype'] 		= $this->input->get('ctype');
+
+		$a['cetak']	= $this->laporan_advance->get_cetak($lokasi, $sublokasi, $id_golongan, $status_pegawai, $jenis_kelamin);
+		//var_dump($cetak); die;
+		$a['fungsi'] = $this->load->library('func_table');
+//				------ 
+		$pdfFilePath = "Rekap_pegawai";
+		
+		if($ctype=='excel'){
+			$this->load->view('dashboard_admin/laporan/advance/cetak_advance', $a);
+		} else {
+			ini_set("pcre.backtrack_limit", "100000000");
+			$mpdf = new \Mpdf\Mpdf();
+			//$html = $this->load->view('dashboard_admin/tunjangan/export_digital', $d, true);
+			$html = $this->load->view('dashboard_admin/laporan/advance/cetak_advance', $a, true);
+			$mpdf->AddPage('L', '', '', '', '', 10, 10, 10, 10, 18, 12);
+			$mpdf->simpleTables = true;
+			$mpdf->packTableData = true;
+			$mpdf->SetTitle($pdfFilePath);
+
+
+
+			$mpdf->WriteHTML($html);
+			$mpdf->Output($pdfFilePath . '.pdf', 'I');
+		}
+		
+	} #end function cetak
 }
 
 /* End of file laporan_advance.php.php */
